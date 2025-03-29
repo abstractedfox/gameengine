e8a30292ad95cbf07a734c0611369fe94ae51a67
Chris <chriswhoprograms@gmail.com>
2025-03-28 23:49:07 -0400

    library groundwork

---
+++ flask/static/matrix.js
@@ -0,0 +1,8 @@
+//Operations on array buffers or other things that are shaped like matricies
+
+//Return a smaller section of a matrix
+//matrix, matrix_x, matrix_y: source matrix & its dimensions
+//section_x, section_y, section_w, section_h: the origin point & dimensions of the section
+function matrix_section(matrix, matrix_x, matrix_y, section_x, section_y, section_w, section_h){
+    //do this
+}
