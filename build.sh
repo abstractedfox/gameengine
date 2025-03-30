cc48d1042d0fdbdfb2576a3820817b5bffcdc9f1
Chris <chriswhoprograms@gmail.com>
2025-03-30 19:24:56 -0400

    build script updating

--- build.sh
+++ build.sh
@@ -1,8 +1,32 @@
-#Pass path to game root directory containing 'flask' as 1st argument
-
 set -e
+run=false
 
-echo "Making destination 'out' directory. Will fail if this directory already exists"
+while [[ $# -gt 1 ]]; do
+    case $1 in
+        -help|-h)
+            echo "start.sh help:"
+            echo "start.sh [args] /path/to/root/directory/of/game/repo/"
+            echo "-clean / -c: Remove the existing 'out' directory before building"
+            echo "-run / -r: Run immediately after building"
+            echo "-help / -h: Display this message"
+            shift 
+            ;;
+
+        -clean|-c)
+            echo "Building clean"
+            rm -rf out
+            shift
+            ;;
+        
+        -run|-r)
+            run=true 
+            shift
+            ;;
+    
+    esac
+done
+
+echo "Making destination 'out' directory."
 mkdir out
 
 echo "Using existing platform as base"
@@ -10,3 +34,8 @@ cp -r ./flask out
 
 echo "Copying game files from within $1/flask"
 cp -rf $1/flask/ ./out/flask/
+
+if [ $run ]; then
+    cd ./out/flask/
+    ./start.sh
+fi
