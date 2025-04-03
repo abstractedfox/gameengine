e03b5aa66ef229d3dbfbe5c4e5f208af6da3a161
Chris <chriswhoprograms@gmail.com>
2025-04-03 15:27:04 -0400

    more audio

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,20 @@
 from flask import Flask
 from flask import render_template
+import os
 
 app = Flask(__name__, static_folder = "static")
 
 @app.route("/")
 def index():
     return render_template("/index.html")
+
+@app.route("/audio")
+def getAudioFiles():
+    result = []
+    extensions = [".mp3", ".wav"]
+    for file in os.listdir("./static/program"):
+        print(file)
+        if file[-4:] in extensions:
+            result.append(file)
+
+    return result
